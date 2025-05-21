b2ffc9ba0e3c4f611d947c53f8079d31b959034b
Akshaya2k5 <166425815+Akshaya2k5@users.noreply.github.com>
2025-05-21 15:27:33 +0530

    Update solution.c

--- solutions/solution.c
+++ solutions/solution.c
@@ -10,45 +10,8 @@ struct TreeNode {
     struct TreeNode *right;
 };
 
-// Helper to create new nodes
-struct TreeNode* newNode(int val) {
-    struct TreeNode* node = (struct TreeNode*)malloc(sizeof(struct TreeNode));
-    node->val = val;
-    node->left = node->right = NULL;
-    return node;
-}
-
-// Find the minimum value node in a subtree
-struct TreeNode* findMin(struct TreeNode* node) {
-    while (node && node->left != NULL) {
-        node = node->left;
-    }
-    return node;
-}
-
 // Function to delete a node from BST
 struct TreeNode* deleteNode(struct TreeNode* root, int key) {
-    if (root == NULL) return NULL;
-
-    if (key < root->val) {
-        root->left = deleteNode(root->left, key);
-    } else if (key > root->val) {
-        root->right = deleteNode(root->right, key);
-    } else {
-        if (root->left == NULL) {
-            struct TreeNode* temp = root->right;
-            free(root);
-            return temp;
-        } else if (root->right == NULL) {
-            struct TreeNode* temp = root->left;
-            free(root);
-            return temp;
-        }
-
-        struct TreeNode* temp = findMin(root->right);
-        root->val = temp->val;
-        root->right = deleteNode(root->right, temp->val);
-    }
-
-    return root;
+    // TODO: Implement this function
+    return NULL;
 }
