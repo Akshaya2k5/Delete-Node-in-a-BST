c418422ce1e5ee462ddaa844e5212c2f2a2d459b
Akshaya2k5 <166425815+Akshaya2k5@users.noreply.github.com>
2025-05-21 15:26:17 +0530

    Update Solution.java

--- solutions/Solution.java
+++ solutions/Solution.java
@@ -15,6 +15,30 @@ public class Solution {
     }
 
     public TreeNode deleteNode(TreeNode root, int key) {
-        // write your code 
+        if (root == null) return null;
+
+        if (key < root.val) {
+            root.left = deleteNode(root.left, key);
+        } else if (key > root.val) {
+            root.right = deleteNode(root.right, key);
+        } else {
+            // Node found
+            if (root.left == null) return root.right;
+            else if (root.right == null) return root.left;
+            else {
+                // Find inorder successor (smallest in right subtree)
+                TreeNode successor = findMin(root.right);
+                root.val = successor.val;
+                root.right = deleteNode(root.right, successor.val);
+            }
+        }
+        return root;
+    }
+
+    private TreeNode findMin(TreeNode node) {
+        while (node.left != null) {
+            node = node.left;
+        }
+        return node;
     }
 }
