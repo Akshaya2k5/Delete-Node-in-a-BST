471d968134a8f7ee5734fefa7897699a8278ee87
Akshaya2k5 <166425815+Akshaya2k5@users.noreply.github.com>
2025-05-21 15:13:38 +0530

    Update solution.py

--- solutions/solution.py
+++ solutions/solution.py
@@ -6,25 +6,5 @@ class TreeNode:
 
 class Solution:
     def deleteNode(self, root, key): 
-        if not root:
-            return None 
-        if key < root.val:
-            root.left = self.deleteNode(root.left, key)
-        elif key > root.val:
-            root.right = self.deleteNode(root.right, key)
-        else:
-            if not root.left:
-                return root.right
-            elif not root.right: 
-                return root.left
-
-            min_larger_node = self.getMin(root.right)
-            root.val = min_larger_node.val
-            root.right = self.deleteNode(root.right, root.val)
-        return root
-
-    def getMin(self, node):
-        while node.left:
-            node = node.left
-        return node
+        pass
 
